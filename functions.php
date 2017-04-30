aa542505011a2825230262fef3cc1b58fe8b623f
Angel Angeles <angelangelesiii@gmail.com>
2017-04-30 15:07:43 +0800

    Changed menu-1 display value

--- functions.php
+++ functions.php
@@ -83,7 +83,7 @@ function ccgm_setup() {
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'menu-1' => esc_html__( 'Primary', 'ccgm' ),
+		'menu-1' => esc_html__( 'Header Main Menu', 'ccgm' ),
 	) );
 
 	/*
