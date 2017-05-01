4052aff223f43ac9dfb8d86dee54c1e1983abb60
Angel Angeles <angelangelesiii@gmail.com>
2017-05-01 13:53:26 +0800

    Added navbar toggling

--- js/main.js
+++ js/main.js
@@ -5,6 +5,36 @@
 
 jQuery(document).ready(function($){ // Document Ready
 
+	// ===================================================
+	//  ScrollMagic Controller
+	// ===================================================
 
+	var mainController = new ScrollMagic.Controller();
+
+
+	// ===================================================
+	//  Navbar Toggling
+	// ===================================================
+
+	var navScene = new ScrollMagic.Scene({
+		triggerElement: 'main#main',
+		triggerHook: 0,
+		offset: 50
+	})
+	.on('enter', function() { // if viewport moved by 75px, remove 'top-position' class
+		$('#ccgm-header.front-page').removeClass('top-position');
+	})
+	.on('leave', function() {// if viewport is < 75px from top, add 'top-position' class
+		$('#ccgm-header.front-page').addClass('top-position');
+	})
+	// .addIndicators()
+	.addTo(mainController);
+
+
+	// ===================================================
+	//  Header Spacer for not front-page pages
+	// ===================================================
+
+	$('.header-spacer').height($('#ccgm-header').outerHeight());
 
 });
