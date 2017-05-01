a233727e45c94aa5b110c6013a33794f2b4c5772
Angel Angeles <angelangelesiii@gmail.com>
2017-05-01 18:58:17 +0800

    Added welcome section

--- front-page.php
+++ front-page.php
@@ -40,6 +40,14 @@ get_header(); ?>
 
 		</section>
 
+		<section class="welcome">
+			<div class="content wrapper">
+				<h2 class="welcome-text"><span class="Christ">Christ</span> <span class="changing"></span></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero sapiente, ab dolore amet nulla totam beatae dolorum quaerat. Optio dolore nemo aperiam nihil, assumenda saepe, repudiandae magni, dolor a ipsa perspiciatis.</p>
+				<a href="#" class="btn btn-transparent-gray btn-expanding">Know more about Christ</a>
+			</div>
+		</section>
+
 		<section class="sample">
 			<div class="wrapper">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cupiditate deleniti impedit temporibus dolore iusto, a eius dolorum soluta, et quis sunt architecto accusamus pariatur. Nostrum incidunt maxime, ab libero ex delectus impedit quia fugit modi! Perferendis voluptate incidunt voluptatum iure earum, culpa nisi dolor accusantium ad. Consectetur, porro, aperiam? Libero id magni, quidem, voluptate possimus quas dignissimos dolores ab, facilis perferendis atque iure blanditiis. Quisquam saepe voluptatem velit laboriosam natus iste omnis earum vel vitae est reiciendis asperiores, distinctio nisi sint, adipisci qui aut sed. Tempora totam, explicabo placeat fugiat omnis ducimus aliquam suscipit ut repellat unde! Dolorem nulla error repudiandae ipsum tenetur! Assumenda architecto magnam at accusamus necessitatibus ipsum, sunt! Quidem quam, numquam temporibus ullam praesentium eaque excepturi unde aliquid repellendus, vel. Animi deserunt, libero quae repudiandae quisquam omnis odit. Ratione velit assumenda maiores, laboriosam numquam maxime eius impedit repudiandae, laborum facilis dignissimos! Autem pariatur, doloremque illum possimus, aliquid natus similique iusto veniam saepe, enim accusantium! Maxime enim incidunt quasi ex dicta eaque voluptate veniam quis laborum repellendus optio, quas deleniti doloremque, minima mollitia, voluptatibus architecto assumenda minus necessitatibus, suscipit quo totam? Rerum qui nihil commodi amet animi nam eligendi itaque at, laboriosam iure quos eos, tenetur labore eum atque sapiente beatae ducimus sed nobis explicabo vitae odit?</div>
 		</section>
