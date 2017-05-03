9c84fe204790745cda4b96f9cd95ba196638642c
Angel Angeles <angelangelesiii@gmail.com>
2017-05-03 16:14:05 +0800

    Added wrapper to woocommerce-page

--- woocommerce.php
+++ woocommerce.php
@@ -2,9 +2,11 @@
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php woocommerce_content(); ?>
-
+			
+			<div class="wrapper-medium">
+				<?php woocommerce_content(); ?>
+			</div>
+			
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
