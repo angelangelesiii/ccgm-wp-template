2678b509431286ce2bc7c05a7a61e83ee2c95df6
Angel Angeles <angelangelesiii@gmail.com>
2017-05-03 21:10:34 +0800

    Removed woocommerce.php for custom templating

--- woocommerce_bak.php
+++ woocommerce_bak.php
@@ -2,8 +2,8 @@
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			
-			<div class="wrapper-medium">
+	
+			<div class="wrapper-medium woocommerce-container">
 				<?php woocommerce_content(); ?>
 			</div>
 			
