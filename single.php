1cc98c8a9c926d4e91c29b361b9cfcb5e76f2f1f
Angel Angeles <angelangelesiii@gmail.com>
2017-05-03 11:26:40 +0800

    Added single post layout

--- single.php
+++ single.php
@@ -10,26 +10,54 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main single-page-post" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+		<?php while(have_posts()): the_post(); ?>
 
-			get_template_part( 'template-parts/content', get_post_format() );
+			<?php 
 
-			the_post_navigation();
+			$headerBackground = '';
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			if(has_post_thumbnail()) $headerBackground = 'style=" background-image: url(\''.get_the_post_thumbnail_url().'\'); "';
 
-		endwhile; // End of the loop.
-		?>
+			?>
+			
+			<div class="wrapper">
+				
+				<article class="post-article">
+					
+					<header class="article-header" <?php echo $headerBackground; ?>>
+
+						<div class="overlay"></div>
+
+						<div class="search-form"><?php get_template_part( 'template-parts/search-template-small' ); ?></div>
+
+						<div class="content">
+							<h1 class="article-title"><?php the_title(); ?></h1>
+							<div class="article-meta">
+								<p>Written by <?php the_author(); ?> on <?php the_date(); ?></p>
+							</div>
+						</div>
+
+						<img src="<?php echo get_avatar_url( get_the_author_meta( 'ID' ) , 64 ); ?>" alt="" class="author-avatar">
+
+					</header>
+					
+					<div class="article-content">
+						<?php the_content(); ?>
+					</div>
+
+				</article>
+
+			</div>
+
+		<?php endwhile; ?>
 
 		</main><!-- #main -->
+
+		<?php get_template_part( 'template-parts/fp-section-activity' ); ?>
+
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
---
+++ template-parts/search-template-small.php
@@ -0,0 +1,5 @@
+<div class="small-search-form">
+	<div class="container">
+		<?php get_search_form(); ?>
+	</div>
+</div>
