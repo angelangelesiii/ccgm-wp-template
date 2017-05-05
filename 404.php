c268338d8014b188de7b5c3fae919249f0ee3d4a
Angel Angeles <angelangelesiii@gmail.com>
2017-05-05 20:33:38 +0800

    Fixed 404 page

--- 404.php
+++ 404.php
@@ -13,49 +13,19 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ccgm' ); ?></h1>
-				</header><!-- .page-header -->
+				<div class="wrapper">
+					<img src="<?php echo get_template_directory_uri().'/images/display/tomb-gray.png' ?>" alt="Tomb" class="tomb">
+					<h1 class="not-found-text">The one you are looking for is not here!</h1>
+					<p class="not-found-desc">Either the page you are looking for does not exist, have been moved or is deleted.</p>
+					<p class="salvation">Is it salvation that you are looking for? We have the answers for you. Come visit us at one of our schedules!</p>
+				</div>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ccgm' ); ?></p>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( ccgm_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ccgm' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ccgm' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
+
+			<?php get_template_part( 'template-parts/fp-section-posts' ); ?>
+
+			<?php get_template_part( 'template-parts/fp-section-activity' ); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
