bffe00f9d6b56a1e061e79b2cc2eb28ffc615ee4
Angel Angeles <angelangelesiii@gmail.com>
2017-05-08 11:15:01 +0800

    Added custom page template suitable for page builder

---
+++ templates/template-no-wrapper-content.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package CCGM_Theme_by_Zimit_Media
+ */
+
+/*
+Template Name: Custom Page
+*/
+
+get_header(); ?>
+
+<?php 
+$backgroundColor = '';
+$fontColor = '';
+if (get_field('background_color')) $backgroundColor = 'style="background-color: '.get_field('background_color').';"';
+if (get_field('font_color')) $fontColor = 'style="color: '.get_field('font_color').';"';
+?>
+
+	<div id="primary" class="content-area" <?php echo $backgroundColor ?>>
+		<main id="main" class="site-main page" role="main">
+
+			<?php while(have_posts()): the_post(); ?>
+
+			<?php 
+
+			$headerBackground = '';
+
+			if(has_post_thumbnail()) $headerBackground = 'style=" background-image: url(\''.get_the_post_thumbnail_url().'\'); "';
+
+			?>
+			
+			<article <?php post_class( array( 'page-article', 'template-custom-page' )); ?>>
+
+				<?php if(get_field('header_bar_boolean')) : ?>
+
+				<header class="page-header<?php if(has_post_thumbnail()) echo ' has-post-thumbnail'; ?>" <?php echo $headerBackground ?>>
+
+					<?php if(has_post_thumbnail()) : ?>
+					<div class="overlay"></div>
+					<?php endif; ?>
+
+					<div class="content">
+						<div class="wrapper">
+							<h1 class="page-title"><?php the_title(); ?></h1>
+							<?php edit_post_link('<i class="fa fa-pencil" aria-hidden="true"></i> Edit this'); ?>
+						</div>
+					</div>
+
+
+				
+				</header>
+
+				<div class="no-wrapper">
+
+					<div class="page-content" <?php echo $fontColor; ?>>
+						<?php if(get_the_content()):
+							the_content();
+						else:
+						?>
+						<p class="no-content-found">
+							Coming Soon!
+						</p>
+						<?php endif; ?>
+					</div>
+				
+				</div>
+
+			</article>
+
+			<?php else : ?>	
+				<div class="wrapper edit-link-container">
+					<?php edit_post_link('<i class="fa fa-pencil" aria-hidden="true"></i> Edit this'); ?>
+				</div>
+			<?php endif; ?>
+
+		<?php endwhile; ?>
+		
+		<?php if(get_field('activity_section_boolean')) get_template_part( 'template-parts/fp-section-activity' ); ?>
+		
+		</main><!-- #main -->
+		
+		
+
+	</div><!-- #primary -->
+
+<?php
+get_footer();
